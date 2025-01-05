5c356fc676be2f0f5f5e00b0be414a8cac4facd3
Mrinmoy <mrinmoyd629@gmail.com>
2025-01-06 02:13:03 +0600

    Set Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 
 function App() {
 
@@ -9,7 +10,11 @@ function App() {
     <>
       
       <Header></Header>
+      <div className="md:flex">
       <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
+   
       
     </>
   )
